60af0b3bdc5906f646198a00ec3276b421f19d3c
illegal-instuction-co <79029454+illegal-instuction-co@users.noreply.github.com>
2021-02-17 11:39:25 +0300

    Correctify samples

--- examples/example.js
+++ examples/example.js
@@ -5,23 +5,23 @@ var BEA256 = require('./BEA256')
 */
 
 var base64en = new BEA256("MERHABALAR AQ", "test").encrypt("base64")
-var base64de = new BEA256(base64en, "test").decrypt("base64")
+// var base64de = new BEA256(base64en, "test").decrypt("base64")
 
-console.log("Base64:\n", blockde);
+console.log("Base64:\n", base64en);
 
 
 /*
   Get results json
 */
 var jsonen = new BEA256("MERHABALAR AQ", "test").encrypt("json")
-var jsonde = new BEA256(jsonen, "test").decrypt("json")
+// var jsonde = new BEA256(jsonen, "test").decrypt("json")
 
-console.log("Json:\n", blockde);
+console.log("Json:\n", jsonen);
 
 /*
   Get results block
 */
 var blocken = new BEA256("MERHABALAR AQ", "test").encrypt()
-var blockde = new BEA256(blocken, "test").decrypt()
+// var blockde = new BEA256(blocken, "test").decrypt()
 
-console.log("Block:\n", blockde);
+console.log("Block:\n", blocken);
